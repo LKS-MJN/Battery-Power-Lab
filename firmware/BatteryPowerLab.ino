79b73c0fce0a9d60db91f59b754ec1935102efe5
LKS-MJN <contact.lakshaymahajan@gmail.com>
2026-06-23 23:04:23 +0530

    testing

--- firmware/BatteryPowerLab.ino
+++ firmware/BatteryPowerLab.ino
@@ -1,12 +1,19 @@
-const int analogPin = A0;
-
-void setup() {
-    serial.begin(9600);
-}
-
 void loop() {
-    int raw = analogRead(analogPin);
-    Serial.println(raw);
-    delay(1000);
+  int raw = analogRead(analogPin);
+  float vRef = 5.0;                    // Arduino reference voltage
+  float vPin = (raw / 1023.0) * vRef;  // voltage at A0
 
+  const float R1 = 1000.0;             // ohms
+  const float R2 = 220.0;              // ohms
+  float vBat = vPin * (R1 + R2) / R2;  // estimated battery voltage
+
+  Serial.print("Raw: ");
+  Serial.print(raw);
+  Serial.print("  Vpin: ");
+  Serial.print(vPin, 3);
+  Serial.print(" V  Vbat: ");
+  Serial.print(vBat, 3);
+  Serial.println(" V");
+
+  delay(1000);
 }
